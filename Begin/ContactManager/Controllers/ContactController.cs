b46fd81bc25343fb2f4f6708f00182278a6a29c7
Matija Rajcevac <duro.durkan@gmail.com>
2020-12-15 17:44:48 +0100

    Start of Lv3

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using ContactManager.Models;
+using System.Web.Http;
+using ContactManager.Services;
+using System.Net.Http;
+
+namespace ContactManager
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        // GET api/<controller>
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+    }
+}
